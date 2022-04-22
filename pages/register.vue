690ea50f0d0bdf712d4f3173b9232fd4c76ab632
Onai23 <onai.nadapdap.23@gmail.com>
2022-04-22 15:35:09 +0700

    Konversi halaman upload photo

--- pages/register.vue
+++ pages/register.vue
@@ -6,8 +6,34 @@
         <div class="w-auto md:w-2/4 lg:w-2/3 flex justify-center items-center">
           <div class="w-full lg:w-1/2 px-10 lg:px-0">
             <h2 class="font-normal mb-6 text-3xl text-white">
-              Sign In to Your Account
+              Sign Up Account
             </h2>
+            <div class="mb-6">
+              <div class="mb-4">
+                <label class="font-normal text-lg text-white block mb-3"
+                  >Full Name</label
+                >
+                <input
+                  type="text"
+                  class="auth-form focus:outline-none focus:bg-purple-hover focus:shadow-outline focus:border-purple-hover-stroke focus:text-gray-100"
+                  placeholder="Write Your Name Here"
+                  value="Julia Keeva Hanna"
+                />
+              </div>
+            </div>
+            <div class="mb-6">
+              <div class="mb-4">
+                <label class="font-normal text-lg text-white block mb-3"
+                  >Occupation</label
+                >
+                <input
+                  type="text"
+                  class="auth-form focus:outline-none focus:bg-purple-hover focus:shadow-outline focus:border-purple-hover-stroke focus:text-gray-100"
+                  placeholder="Write your occupation here"
+                  value="Graphic Designer"
+                />
+              </div>
+            </div>
             <div class="mb-6">
               <div class="mb-4">
                 <label class="font-normal text-lg text-white block mb-3"
@@ -29,7 +55,7 @@
                 <input
                   type="password"
                   class="auth-form focus:outline-none focus:bg-purple-hover focus:shadow-outline focus:border-purple-hover-stroke focus:text-gray-100"
-                  placeholder="Write your password here"
+                  placeholder="Type your password here"
                   value="nasigorenglimaribbu"
                 />
               </div>
@@ -37,18 +63,18 @@
             <div class="mb-6">
               <div class="mb-4">
                 <button
-                  @click="$router.push({ path: '/' })"
+                  @click="$router.push({ path: '/upload' })"
                   class="block w-full bg-orange-button hover:bg-green-button text-white font-semibold px-6 py-4 text-lg rounded-full"
                 >
-                  Sign In
+                  Continue Sign Up
                 </button>
               </div>
             </div>
             <div class="text-center">
               <p class="text-white text-md">
-                Don't have account?
-                <nuxt-link to="/register" class="no-underline text-orange-button"
-                  >Sign Up
+                Already have account?
+                <nuxt-link to="/login" class="no-underline text-orange-button"
+                  >Sign In
                 </nuxt-link>.
               </p>
             </div>
@@ -57,17 +83,15 @@
       </div>
 </template>
 
-// export layout from auth.vue
-
 <script>
 export default {
     layout: 'auth'
 }
 </script>
 
-<style scoped>
+<style>
 .auth-background {
-    background-image: url("/sign-in-background.jpg");
+    background-image: url("/sign-up-background.jpg");
     background-position: center;
     background-size: cover;
 }
